a29a8c2acba6adeb57efe0df3c99bad2f121cd71
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-14 15:09:54 -0700

    creates working routes, creates base compoent structure

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,6 @@
+export default function Dashboard() {
+
+    return (
+        <h1> This is the Dashboard component</h1>
+    )
+}
---
+++ src/pages/Stock.jsx
@@ -0,0 +1,6 @@
+export default function Stock() {
+
+    return (
+        <h1> This is the Stock component</h1>
+    )
+}
