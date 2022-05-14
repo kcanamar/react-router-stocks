cf997cac2bbbb37e89a3a5b4dea2265c3702478d
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-14 15:56:02 -0700

    creates data template on dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,25 @@
+import { Link } from "react-router-dom"
+import Data from "../components/stock-data.js"
+
 export default function Dashboard() {
 
     return (
-        <h1> This is the Dashboard component</h1>
+        <div>
+            <h2>Stocks</h2>
+            <div className="container">
+                <div>Company Name</div>
+                <div>Price</div>
+                <div>Change</div>
+                <hr/>
+                {Data.map(({ name, symbol, change, open, lastPrice}, idx) =>
+                    <Link key={idx} to={`/stocks/${symbol}`}>
+                        <div>{name}</div>
+                        <div>{lastPrice}</div>
+                        <div className={parseFloat(change).toFixed(2) >= 0 ? "green" : "red"}>{parseFloat(open).toFixed(2)}({(parseFloat(change).toFixed(2) >= 0 ? `+${parseFloat(change).toFixed(2)}` : parseFloat(change).toFixed(2))})</div>
+                        <hr/>
+                    </Link>
+                )}
+            </div>
+        </div>
     )
 }
