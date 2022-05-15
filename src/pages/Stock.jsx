571da01ab42d6602d4c648c3bcb84d1bfe833c48
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-14 17:38:37 -0700

    creates stock display page

--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import Data from "../components/stock-data"
+
 export default function Stock() {
 
+    const { symbol } = useParams()
+    const [stock, setStock] = useState(Data)
+    function getStock() {
+        let newStock = Data.find((stocks) => stocks.symbol.includes(symbol))
+        setStock(newStock)
+    }
+
+    useEffect(() => {
+        getStock()
+    }, [])
+
     return (
-        <h1> This is the Stock component</h1>
+        <div> 
+            <h1>Trading Symbol --- ({symbol})</h1>
+            <div className="stock-container">
+                <h2>{stock.name}</h2>
+                <h2>Last Price --- {stock.lastPrice}</h2>
+                <h2 className={parseFloat(stock.change).toFixed(2) >= 0 ? "green" : "red"}>Current Change --- ({(parseFloat(stock.change).toFixed(2) >= 0 ? `+${parseFloat(stock.change).toFixed(2)}` : parseFloat(stock.change).toFixed(2))})</h2>
+                <h2>Today's High --- {stock.high}</h2>
+                <h2>Today's Low --- {stock.low}</h2>
+                <h2>Opening --- {stock.open}</h2>
+            </div>
+        </div>
     )
 }
