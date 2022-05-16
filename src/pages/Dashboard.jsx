cdb8d84b5c5f25ee9f45a5036d24249147375f0b
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-16 10:10:57 -0700

    rearanges table

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -21,29 +21,25 @@ export default function Dashboard() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>
-                                {Data.map(({ name, symbol }, idx) =>
-                                    <Link key={idx} to={`/stocks/${symbol}`}>
+                        {Data.map(({ name, symbol, change, lastPrice, open }, idx) =>
+                            <tr key={idx}>
+                                <td>
+                                    <Link to={`/stocks/${symbol}`}>
                                             <div className="company-name">{name}</div>
                                     </Link>
-                                )}
-                            </td>
-                            <td>
-                                {Data.map(({ symbol, lastPrice}, idx) =>
-                                        <Link key={idx} to={`/stocks/${symbol}`}>
+                                </td>
+                                <td>
+                                    <Link to={`/stocks/${symbol}`}>
                                         <div className="price">{lastPrice}</div>
                                     </Link>
-                                )}
-                            </td>
-                            <td>
-                                {Data.map(({symbol, change, open}, idx) =>
-                                        <Link key={idx} to={`/stocks/${symbol}`}>
-                                        <div className={parseFloat(change).toFixed(2) >= 0 ? "green change" : "red change"}>{parseFloat(open).toFixed(2)}({(parseFloat(change).toFixed(2) >= 0 ? `+${parseFloat(change).toFixed(2)}` : parseFloat(change).toFixed(2))})</div>
+                                </td>
+                                <td>
+                                    <Link to={`/stocks/${symbol}`}>
+                                        <div className={parseFloat(change).toFixed(2) >= 0 ? "green change" : "red change"}>{parseFloat(open).toFixed(2)} ({(parseFloat(change).toFixed(2) >= 0 ? `+${parseFloat(change).toFixed(2)}` : parseFloat(change).toFixed(2))})</div>
                                     </Link>
-                                )}
-                            </td>
-                        </tr>
+                                </td>
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
